b0962a15ed7b624b2cdbd6d3f101b5d8d3d3a4db
Susha Dore and Slavik Mygovych <slavik@>
2016-09-19 20:41:04 -0700

    update amusement park to include height using and/or

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,20 @@ $(document).ready(function(){
 
   $("#blanks form").submit(function(event) {
     var age = $("input#ageQ").val();
+    var height = $("input#heightQ").val();
 
-    if (age < 10) {
+    if (age < 10 || height < 36) {
       $("#tooYoung").show();
       $("#rcoaster").hide();
       $("#dzone").hide();
       $("#corkscrew").hide();
-    } else if (age >= 10 && age < 12) {
+    } else if (age >= 10 && age < 12 && height >= 36 && height <= 42) {
+      $("#tooYoung").hide();
       $("#rcoaster").show();
+      $("#dzone").hide();
       $("#corkscrew").hide();
-    } else if (age >= 12 && age < 14) {
+    } else if (age >= 12 && age < 14 && height > 42 && height <= 48) {
+      $("#tooYoung").hide();
       $("#rcoaster").show();
       $("#dzone").show();
       $("#corkscrew").hide();
@@ -21,7 +25,6 @@ $(document).ready(function(){
       $("#dzone").hide();
       $("#corkscrew").show();
     }
-
 
     event.preventDefault();
   });
